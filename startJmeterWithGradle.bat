525a8b97c11f0f3b6525a0ef15edc8a5cd0898bf
Jon Austen <jon.austen@healthsparq.com>
2015-04-06 14:44:01 -0700

    Fix .bat scripts so they dont close immediately on error.

--- startJmeterWithGradle.bat
+++ startJmeterWithGradle.bat
@@ -5,6 +5,10 @@ ECHO Closing this window will close Jmeter...
 IF NOT DEFINED GRADLE_HOME ECHO It appears you are missing the Gradle installation on your system.
 IF NOT DEFINED JAVA_HOME ECHO You need to define JAVA_HOME on your system.
 
-gradle jmeterEditor %1 %2 %3
+CALL gradle jmeterEditor %1 %2 %3
 
-pause
+::-------------------------------------------------------------------
+::  End script
+::-------------------------------------------------------------------
+:END
+FOR /l %%a IN (10,-1,1) DO (TITLE %WINDOW_TITLE% -- Closing in %%as&PING.exe -n 2 -w 1 127.0.0.1>nul)
--- startJmeterWithMaven.bat
+++ startJmeterWithMaven.bat
@@ -5,7 +5,10 @@ ECHO Closing this window will close Jmeter...
 IF NOT DEFINED M2_HOME ECHO It appears you are missing the Maven installation on your system.
 IF NOT DEFINED JAVA_HOME ECHO You need to define JAVA_HOME on your system.
 
-call setJavaHome.bat
-mvn.bat jmeter:gui
+CALL mvn.bat jmeter:gui
 
-pause
+::-------------------------------------------------------------------
+::  End script
+::-------------------------------------------------------------------
+:END
+FOR /l %%a IN (10,-1,1) DO (TITLE %WINDOW_TITLE% -- Closing in %%as&PING.exe -n 2 -w 1 127.0.0.1>nul)
