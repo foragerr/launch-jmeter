f6b6c333b08cdef5fcddb683db2bde6c272ca325
Jon Austen <djangofan@gmail.com>
2015-04-07 19:41:37 -0700

    Got jmeter plugins all to load in jmeter 2.13!

--- startJmeterWithGradle.bat
+++ startJmeterWithGradle.bat
@@ -5,10 +5,10 @@ ECHO Closing this window will close Jmeter...
 IF NOT DEFINED GRADLE_HOME ECHO It appears you are missing the Gradle installation on your system.
 IF NOT DEFINED JAVA_HOME ECHO You need to define JAVA_HOME on your system.
 
-CALL gradle jmeterEditor %1 %2 %3
+CALL gradle clean addToJmeterClasspath jmeterEditor --info %1 %2 %3
 
 ::-------------------------------------------------------------------
 ::  End script
 ::-------------------------------------------------------------------
 :END
-FOR /l %%a IN (10,-1,1) DO (TITLE %WINDOW_TITLE% -- Closing in %%as&PING.exe -n 2 -w 1 127.0.0.1>nul)
+FOR /l %%a IN (60,-1,1) DO (TITLE %WINDOW_TITLE% -- Closing in %%as&PING.exe -n 2 -w 1 127.0.0.1>nul)
